a3d58c179a0b45f08221ee494e8c202bc00ebfd0
ap-aditya <146554662+ap-aditya@users.noreply.github.com>
2024-07-25 10:03:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     rollupOptions: {
-      input: '../index.html', 
+      input: resolve(__dirname, 'index.html'), 
     }
   },
   server: {
