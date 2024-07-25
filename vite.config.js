3ece79bf787ee6d4c87c9b7c9441f9dd0b9add14
ap-aditya <146554662+ap-aditya@users.noreply.github.com>
2024-07-25 09:48:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'src',  
   build: {
-    outDir: 'dist',
+    outDir: '../dist',  
   },
   server: {
     proxy: {
@@ -15,5 +15,5 @@ export default defineConfig({
         secure: false,
       }
     }
-  },
-})
+  }
+});
